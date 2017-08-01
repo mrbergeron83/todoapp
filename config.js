0a75322ea57fc37de604357889a7442d5ea3ac6b
mrbergeron83 <mr.bergeron83@gmail.com>
2017-08-01 19:14:25 -0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-var config = {
-    url: "mongodb://mrbergeron83:Qed5xOo9IydG79AavhTr7ZgvV9tyB61RMPfacBDRiET844q3AH4MHUZDuAuPqShiV2vx1GHNndfr89dmo9BXzw==@mrbergeron83.documents.azure.com:10255/?ssl=true",
-    port: process.env.PORT || 3000    
-}
-module.exports = config;
