2b87027f0ecd37fc05636005d0ab2c9e1ee4503d
Michael Bergeron <mr.bergeron83@gmail.com>
2017-08-02 15:55:33 -0400

    added service worker2

--- todo/todo.js
+++ todo/todo.js
@@ -1,11 +1,11 @@
 // Todo.js
 var mongoose = require('mongoose');
-               
+var Moment = require('moment');               
 var TodoSchema = new mongoose.Schema({
   item: String,
   done: { type: Boolean, default: false},
   deleted: { type: Boolean, default: false},
-  date: { type: Date, default: Date.now }
+  date: { type: String, default: new Moment().format('L') }
 });
 mongoose.model('Todo', TodoSchema);
 module.exports = mongoose.model('Todo');
